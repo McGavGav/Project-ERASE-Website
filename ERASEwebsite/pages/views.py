2d176e6e6b52088e0e75ab238fcb1712dede6ad6
gavin <gavin@GAVSBOX>
2026-02-19 14:03:47 -0800

    Basic pages implementation from DJango Template

---
+++ ERASEwebsite/pages/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    """Render the homepage"""
+    return render(request, 'home.html')
+
+
+def about(request):
+    """Render the about page"""
+    return render(request, 'about.html')
+
+
+def contact(request):
+    """Render the contact page"""
+    return render(request, 'contact.html')
