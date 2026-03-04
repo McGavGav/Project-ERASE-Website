ce1830c52c9f38022738e5f389d6d5ff8842b08a
gavin <gavin@GAVSBOX>
2026-03-04 15:15:10 -0800

    Added basic sign in logout functionality

--- ERASEwebsite/pages/urls.py
+++ ERASEwebsite/pages/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 app_name = 'pages'
@@ -7,4 +8,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='pages:home'), name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
