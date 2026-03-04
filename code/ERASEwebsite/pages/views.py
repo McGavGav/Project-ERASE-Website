7d2e3bc8a77c8f23cbbd56ec909c55a90bba8990
gavin <gavin@GAVSBOX>
2026-03-04 15:15:10 -0800

    Added basic sign in logout functionality

--- code/ERASEwebsite/pages/views.py
+++ code/ERASEwebsite/pages/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     """Render the homepage"""
@@ -15,3 +17,21 @@ def about(request):
 def contact(request):
     """Render the contact page"""
     return render(request, 'contact.html')
+
+
+def signup(request):
+    """Handle user sign-up"""
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('pages:login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+class CustomLoginView(LoginView):
+    """Custom login view"""
+    template_name = 'login.html'
+    redirect_authenticated_user = False
