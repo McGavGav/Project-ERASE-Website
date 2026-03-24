47bb7c718ed5e1822a88b3cd7ec1e6d8473c33e8
Gavin A Burchett <gavin.burchett@wsu.edu>
2026-03-24 11:44:10 -0700

    Integrated map into website styling, added link

--- code/ERASEwebsite/pages/urls.py
+++ code/ERASEwebsite/pages/urls.py
@@ -12,8 +12,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='pages:home'), name='logout'),
     path('signup/', views.signup, name='signup'),
     path('calendar/', views.calendar, name='calendar'),
-    path('shipment-map/', views.shipment_map),
+    path('shipment-map/', views.shipment_map, name='shipment_map'),
     path('studentdb/', views.studentdb, name='studentdb'),
     path('manage-users/', views.manage_users, name='manage_users'),
-    path('manage-users/', views.manage_users, name='manage_users')
 ]
