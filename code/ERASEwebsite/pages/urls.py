5040fc8efbc99da90607151328967a2de96d8d3e
Gavin A Burchett <gavin.burchett@wsu.edu>
2026-03-23 18:45:43 -0700

    Super User, Admin, User status implementation

--- code/ERASEwebsite/pages/urls.py
+++ code/ERASEwebsite/pages/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='pages:home'), name='logout'),
     path('signup/', views.signup, name='signup'),
     path('calendar/', views.calendar, name='calendar'),
-    path('studentdb/', views.studentdb, name='studentdb')
+    path('studentdb/', views.studentdb, name='studentdb'),
+    path('manage-users/', views.manage_users, name='manage_users')
 ]
