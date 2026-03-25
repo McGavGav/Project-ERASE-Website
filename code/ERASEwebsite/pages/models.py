833dea61503c4764257602037ccec63a35520063
hdang04 <hunter.dang@wsu.edu>
2026-03-24 19:42:05 -0700

    added temporary database logic for admin view and touched up the UI

---
+++ code/ERASEwebsite/pages/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+class StudentQuerySet(models.QuerySet):
+    def search(self, query):
+        if query:
+            return self.filter(name__icontains=query)
+        return self
+
+    def filter_gender(self, gender):
+        if gender:
+            return self.filter(gender=gender)
+        return self
+
+    def filter_school(self, school):
+        if school:
+            return self.filter(school__icontains=school)
+        return self
+
+    def apply_filters(self, search=None, gender=None, school=None):
+        return (
+            self.search(search)
+            .filter_gender(gender)
+            .filter_school(school)
+        )
+
+class StudentManager(models.Manager):
+    def get_queryset(self):
+        return StudentQuerySet(self.model, using=self._db)
+
+    def apply_filters(self, *args, **kwargs):
+        return self.get_queryset().apply_filters(*args, **kwargs)
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+    school = models.CharField(max_length=200)
+    # add field for student photo later
+
+    objects = StudentManager() 
+
+    def __str__(self):
+        return self.name
--- code/ERASEwebsite/pages/views.py
+++ code/ERASEwebsite/pages/views.py
@@ -13,6 +13,7 @@ import os
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
 from event_calendar.calendar_maker import get_calendar_html
+from .models import Student
 
 def home(request):
     """Render the homepage"""
@@ -90,43 +91,38 @@ def calendar(request):
 
     return render(request, 'calendar.html', context)
 
-
-
 def studentdb(request):
-    # temp data to test database view.
-    students = [
-        {"name": "Anne", "gender": "Female", "school": "Washington State University", "photo": None},
-        {"name": "Tim", "gender": "Male", "school": "Lincoln Primary", "photo": None},
-        {"name": "Emma", "gender": "Female", "school": "Washington Middle School", "photo": None},
-        {"name": "Daniel", "gender": "Male", "school": "Franklin High", "photo": None},
-    ]
-    
-    search_query = request.GET.get("search", "")
-    gender_filter = request.GET.get("gender", "")
-    school_filter = request.GET.get("school", "")
 
-    filtered_students = []
+    # admin/supers can add and delete students
+    if request.method == "POST" and (request.user.is_staff or request.user.is_superuser):
+        action = request.POST.get("action")
 
-    for student in students:
-        if search_query and search_query.lower() not in student["name"].lower():
-            continue
+        if action == "add":
+            Student.objects.create(
+                name=request.POST.get("name"),
+                gender=request.POST.get("gender"),
+                school=request.POST.get("school")
+            )
 
-        if gender_filter and student["gender"] != gender_filter:
-            continue
+        elif action == "delete":
+            Student.objects.filter(
+                name=request.POST.get("student_name")
+            ).delete()
 
-        if school_filter and school_filter.lower() not in student["school"].lower():
-            continue
+        return redirect("pages:studentdb")
 
-        filtered_students.append(student)
+    students = Student.objects.apply_filters(
+        search=request.GET.get("search"),
+        gender=request.GET.get("gender"),
+        school=request.GET.get("school")
+    )
 
-    context = {
-        "students": filtered_students,
-        "search_query": search_query,
-        "gender_filter": gender_filter,
-        "school_filter": school_filter
-    }
-
-    return render(request, "studentdb.html", context)
+    return render(request, "studentdb.html", {
+        "students": students,
+        "search_query": request.GET.get("search", ""),
+        "gender_filter": request.GET.get("gender", ""),
+        "school_filter": request.GET.get("school", "")
+    })
 
 class CustomLoginView(LoginView):
     """Custom login view"""
