c9f306f42d55a5f5e05b879512d25e810c22e407
J-Abitria <146140290+J-Abitria@users.noreply.github.com>
2026-04-30 22:54:09 -0700

    Added new RSVP database.

--- code/ERASEwebsite/event_calendar/forms.py
+++ code/ERASEwebsite/event_calendar/forms.py
@@ -4,8 +4,12 @@ from .models import Event
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ['title', 'date', 'time', 'description']
+        fields = ['title', 'date', 'time', 'description', 'hasRSVP']
+        labels = {
+            'hasRSVP': 'Enable RSVP list?'
+        }
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}),
             'time': forms.TimeInput(attrs={'type': 'time'}),
+            'hasRSVP': forms.CheckboxInput()
         }
--- code/ERASEwebsite/event_calendar/models.py
+++ code/ERASEwebsite/event_calendar/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Event(models.Model):
@@ -5,6 +6,18 @@ class Event(models.Model):
     date = models.DateField()
     time = models.TimeField()
     description = models.TextField(blank=True)
+    hasRSVP = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.title}: {self.date} at {self.time}"
+
+class RSVP(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='rsvps')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rsvps')
+    reserved_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('event', 'user')
+    
+    def __str__(self):
+        return f"{self.user.username} is RSVPed for {self.event.title}"
