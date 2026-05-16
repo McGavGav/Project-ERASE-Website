d11f115c1ecff118c4018e8363985fe79c177395
J-Abitria <146140290+J-Abitria@users.noreply.github.com>
2026-05-16 13:40:34 -0700

    Added new CSS files for web pages.

--- code/ERASEwebsite/mysite/urls.py
+++ code/ERASEwebsite/mysite/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('i18n/set-language/', set_language, name='set_language'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
